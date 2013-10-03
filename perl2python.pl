e418e7457b9f6af2d2e1836000c6dba8e0d69018
roninski <luke@dizzi.net>
2013-10-03 22:39:34 +1000

    priority one plan

---
+++ perl2python.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+
+#	
+#	Plan 
+#	Handle everything as a single string, parsing with ; {} \n boundaries
+#	Step 1: Ensuring correct indentation and handling of {}s and ;s.
+#	If this works, individual statements are isolated in the process,
+#	allowing for better and easier processing of statements later.
+#
