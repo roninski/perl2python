e2c223806662cf68caed948c2fb70ed6cf77a64b
roninski <luke@dizzi.net>
2013-10-08 15:50:42 +1100

    basic string and variable handling

--- perl2python.pl
+++ perl2python.pl
@@ -11,14 +11,30 @@ while ($line = <>){
 @array = split(/#.*\K(?=\n)|(?<=[^\\][;{}])/,$code);
 # Currently not managed: reserved characters between //s and in strings, hashes.
 
+# Main replacements
+foreach $line (@array){
+	# Strip command of heading and trailing whitespace
+	$line =~ s/^\s*//;
+	$line =~ s/\s*$//;
+
+	# Split variables from single strings. 
+	# Only handles single strings
+	if ($line =~ /("[^"]*")/){
+		$x = $1;
+		$x =~ s/[^\\]\K([\$@%]\w*)(?=\W)/"\+str\($1\)\+"/g;
+		$line =~ s/"[^"]*"/$x/;
+	}
+
+	# Handle ++ or -- operations (change to +=1 or -=1)
+	$line =~ s/(^|[^\\])\K([\$@%]\w+)\+\+/$2+=1/g;
+	$line =~ s/(^|[^\\])\K([\$@%]\w+)--/$2-=1/g;
+}
 
 # Handles converting the flow across (indentation, etc)
 # May be best to deal with variables and functions beforehand
 # Puts into final output format (without newlines)
 $indentation = 0;
 foreach $line (@array){
-	$line =~ s/^\s*//;
-	$line =~ s/\s*$//;
 	if ($line =~ /^#!\/usr\/bin\/perl/){
 		$line = "#!/usr/bin/python2.7";
 	} elsif ($line =~ /^#/){
@@ -34,6 +50,13 @@ foreach $line (@array){
 		$indentation--;
 		$line = "\t"x$indentation;
 	}
+}
+
+# Remove dollar signs before arrays and variables.
+# Note: Could in pre-parsing add special characters after the $ or @ to differentiate 
+# from escaped or standard characters
+foreach $line (@array){
+	$line =~ s/(^|[^\\])\K[\$@](?=\w+($|\W))//g;
 }
 
 foreach $line (@array){
